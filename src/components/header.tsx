8a58383385a5015822feac96f92617ebf869b347
Joel Ekpe <JoelEkpe@gmail.com>
2026-01-18 01:03:45 +0000

    CAN YOU MAKE THE SERVICE DROPDOWN TRANSPARENT

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -45,7 +45,7 @@ export function Header() {
                 {link.label} <ChevronDown className="w-4 h-4" />
               </Link>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="start" className="bg-primary text-primary-foreground">
+            <DropdownMenuContent align="start" className="bg-transparent border-none shadow-none">
               {link.submenu.map(sublink => (
                 <DropdownMenuItem key={sublink.label} asChild>
                   <Link href={sublink.href}>{sublink.label}</Link>
