c4ad23e06c4358f4be7dd3e1dbcaa9d553942f37
Joel Ekpe <JoelEkpe@gmail.com>
2026-01-18 00:59:01 +0000

    LET THE SERVICE DROPDOWN BE BLUE AND LETTERS WHITE

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -46,7 +45,7 @@ export function Header() {
                 {link.label} <ChevronDown className="w-4 h-4" />
               </Link>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
+            <DropdownMenuContent align="start" className="bg-primary text-primary-foreground">
               {link.submenu.map(sublink => (
                 <DropdownMenuItem key={sublink.label} asChild>
                   <Link href={sublink.href}>{sublink.label}</Link>
